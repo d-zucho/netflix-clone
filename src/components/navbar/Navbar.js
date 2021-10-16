1fb8d41781535511978d4ff465bae616b71f4cbc
D_Zucho <danny.nico.dev@outlook.com>
2021-10-16 02:26:47 -0700

    header transition test 1

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,35 @@
-// import N from './netflix-N.png'
-// import netlogo from '../../assets/Netflix_Logo_RGB.webp'
-import mainLogo from '../../assets/netflix-logo-back-black.jpeg'
+import { useState, useEffect } from 'react'
+
+import mainLogo from '../../assets/netflix_logo_white.png'
 import avatar from '../../assets/netflix-avatar.png'
 import './navbar.css'
 
 function Navbar() {
+  //? -- Handle Scroll Animation --
+
+  const [show, handleshow] = useState(false)
+  // setting initial value of state (show) to fallse
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleshow(true)
+    } else {
+      handleshow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavbar)
+    return () => {
+      window.removeEventListener('scroll', transitionNavbar)
+    }
+  }, [])
+
   return (
-    <div className="nav">
-      <div className="nav__content">
-        <img src={mainLogo} className="nav__logo" height="100px" alt="Logo" />
-        <img src={avatar} className="nav__avatar" height="100px" alt="Avatar" />
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__contents">
+        <img src={mainLogo} className="nav__logo" alt="Logo" />
+        <img src={avatar} className="nav__avatar" alt="Avatar" />
       </div>
     </div>
   )
